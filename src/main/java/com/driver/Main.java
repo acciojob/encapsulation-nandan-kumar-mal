342725a2cf46afb04f0fa8cbd561141cbb89716d
Nandan <nk1399a@gmail.com>
2022-12-13 20:30:41 +0530

    getter and setter

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String [] args){
+
+        RWOnly r = new RWOnly();
+//        r.name="Nandan";
+//        name has private access in com.driver.RWOnly
+
+        r.setName("Nandan");
+        System.out.println(r.getName());
+    }
 }
